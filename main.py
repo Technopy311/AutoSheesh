cb49f8f3b518c4d4dd0e7475d4492c2e18fa8375
Technopy311 <xmenmagnet@gmail.com>
2022-01-12 22:50:58 -0300

    Script finished

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import os
 
 def saveScans():
     print("Would you like to save the scan?")
-    print("The scan ")
+    print("(The scan will be saved to your current directory)")
     opt = input("(1)Yes (0) No: ")
 
 
     if opt == "1":
-        return(1)
+        add = (" -oN " + os.getcwd() + "AutoSheeshScan.txt") 
+        return(add)
     elif opt == "0":
-        return(0)
+        return('')
     else:
         print("That option does not exist, proceding with save.")
 
@@ -29,33 +30,66 @@ def uidCheck():
         print("Quiting...")
         exit()
     
-    print("Running with this UID: " + str(uid))
+    print("UID: " + str(uid) + ". all good")
 
 
 def Aggressive(target):
-    uidCheck()
-    # save = 
+    save = saveScans()
+    command = "nmap -A --min-rate 5000 -v -n -Pn " + target + save
 
 
 def Beast(target):
-    pass
+    save = saveScans()
+    command = "nmap -sT -sC -O -sV --min-rate 5000 -v -R -Pn " + target + save
+    os.system(command)
 
 
 def Silent(target):
-    pass
+    save = saveScans()
+    command = "nmap -sS -v" + target + " -n -Pn" + save
+    os.system(command)
 
 
 def FastPorts(target):
-    pass
+    save = saveScans()
+    command = "nmap -sS -p- --open -v --min-rate 5000 " + target + " -n -Pn" + save
+    os.system(command)
 
 
 def Custom(target):
-    pass
+    save = saveScans()
+    command = input("Enter your own nmap commands \n:")
+
+    print("Why do u use this option, just write your own nmap in the terminal.\n pfff")
+    exit()
 
 
-def GodScan(target):
-    pass
-# nmap -sS --min-rate 5000 <ip> -n -Pn
+def CrazyScan(target):
+    save = saveScans()
+    command = "nmap -sT -sC -sV -O --script vuln --min-rate 5000 -Pn -n " + target + save
+
+    print("This scan will execute the following:")
+    print(command)
+
+    crazy = input("\nAre you sure that you want to execute this command?\n(1)Yes (2)No: ")
+    
+    try:
+        int(crazy)
+    except TypeError:
+        print("You must enter \"1\" for yes, or \"2\" for No.")
+        CrazyScan()
+    except ValueError:
+        print("You must enter \"1\" for yes, or \"2\" for No.")
+        CrazyScan(target)
+
+    if int(crazy) == 1:
+        print("0.0, you are crazy!!! aaaaagghh *runs away* \n")
+        os.system(command)
+    elif int(crazy) == 2:
+        print("I though that you were Crazy!! Fiuf, cya :P")
+    else:
+        print("That option does not exist, \nplease enter \"1\" for Yes or \"2\" for no")
+        CrazyScan(target)
 
 
 def main():
@@ -66,16 +100,18 @@ def main():
     
     print("\n")
     print("\n")
-    print("Scan Modes:\n(1)Agressive \n(2)Beast \n (3)Silent \n")
-    print("(4)Fazt all ports \n (5)Custom Automation \n(6)The GodScan (scans 4+2) \n")
+    print("Scan Modes:\n(1)Agressive\n(2)Beast\n(3)Silent\n")
+    print("(4)Fazt all open ports\n(5)Custom Automation\n(6)The CrazyScan (scans 4+2)\n")
     print("(0) Exit ")
     print("\n")
     
     mode = input("Enter the selected mode/option: ")
 
+
     try:
         mode = int(mode)
     except ValueError:
+        print("You entered: " + str(mode))
         print("\nYou must enter a number to select an option.")
         print("Quitting...")
         exit()
@@ -85,35 +121,40 @@ def main():
         exit()
 
     if mode == 0:
-        print("Selected mode: " + str(mode) + ", Exit.")    
+        print("\nSelected option: " + str(mode) + ", Exit.")    
         print("\nTy For checking out, cya ;P")
         exit()
 
     elif mode == 1:
-        print("Selected mode: " + str(mode) + ", Aggressive Scan.")    
+        print("\nSelected mode: " + str(mode) + ", Aggressive Scan.\n")    
         Aggressive(target)
         
     elif mode == 2:
-        print("Selected mode: " + str(mode) + ", Beast Scan.")    
+        print("\nSelected mode: " + str(mode) + ", Beast Scan.\n")    
         Beast(target)
 
     elif mode == 3:
-        print("Selected mode: " + str(mode) + ", Silent Scan.")    
+        print("\nSelected mode: " + str(mode) + ", Silent Scan.\n")    
         Silent(target)
 
     elif mode == 4:
-        print("Selected mode: " + str(mode) + ", All ports fast way.")    
+        print("\nSelected mode: " + str(mode) + ", All open ports fast way.\n")    
         print("Not faster than Rustscan tho...")
         FastPorts(target)
 
     elif mode == 5:
-        print("Selected mode: " + str(mode) + ", Your awesome custom scan ;) sheesh.")    
+        print("\nSelected mode: " + str(mode) + ", Your awesome custom scan ;) sheesh.\n")    
         Custom(target)
 
     elif mode == 6:
-        print("Selected mode: " + str(mode) + ", GodScan, please only use this at ctf's")    
-        print("or to make an \"accidental\" DoS.")
-        GodScan(target)
+        print("\nSelected mode: " + str(mode) + ", CrazyScan, please only use this at ctf's")    
+        print("or to make an \"accidental\" DoS. \n")
+        CrazyScan(target)
+    else:
+        print("\n#!! That is Not a valid option, you must enter a number !!# \n")
+        main()
+
+    
     
 
 if __name__ == '__main__':
